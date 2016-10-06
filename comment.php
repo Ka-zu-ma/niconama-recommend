b3f615946d6ec43b8281eda4f71276fe0e409b95
Kazuma Miyazaki <miyazaki@dotinstall.com>
2016-10-06 09:32:37 +0900

    1006_session取得完了

--- comment.php
+++ comment.php
@@ -1,7 +1,10 @@
 <?php
 
 // ログイン画面URL、初め、URLが間違っていて、ログインできていなかったので要注意.
-$url = 'https://account.nicovideo.jp/api/v1/login?show_button_twitter=1&site=niconico&show_button_facebook=1&next_url=';
+$login_url = 'https://account.nicovideo.jp/api/v1/login?show_button_twitter=1&site=niconico&show_button_facebook=1&next_url=';
+
+//プレイヤー情報を得るためのリクエストURL
+$ply_sts_url = 'http://live.nicovideo.jp/api/getplayerstatus?v=lv';
 
 $live_id = '';
 
@@ -14,7 +17,6 @@ $password = '';
 $cookies = [];
 
 $session_id = '';
-
 
 //ログイン処理
 $method = 'POST';
@@ -29,8 +31,8 @@ $query = http_build_query($request, '', '&');
 $header = array(
     "Content-Type: application/x-www-form-urlencoded",
     "Content-Length: " . strlen($query),
-    "Cookie: " . "Cookie: " . $session_id, // ★セッション管理ではセッションIDをクッキーにいれることがほとんどなので一緒に送信
-    "User-Agent: " . "hogehoge", // UserAgentで弾いてるサイトもあるので必要なら適当にセット
+    "Cookie: " . "Cookie: " . $session_id, 
+    "User-Agent: " . "hogehoge", 
 );
 
 $context = array(
@@ -42,19 +44,18 @@ $context = array(
 );
 
 
-// クッキーを取得
-// file_get_contents後に$http_response_headerにレスポンスヘッダが格納されているのでここからクッキー情報を取得
 
-$response = file_get_contents($url, false, stream_context_create($context));
+$response = file_get_contents($login_url, false, stream_context_create($context));
 
 // 存在しないURLにリクエストを出したとき、エラー表示
 if($http_response_header[0] == 'HTTP/1.1 404 Not Found'){
         print '404 Not Foundです。';
 }
 
-var_dump($http_response_header);
+// var_dump($http_response_header);
 
-
+// クッキーを取得
+// file_get_contents後に$http_response_headerにレスポンスヘッダが格納されているのでここからクッキー情報を取得
 foreach ($http_response_header as $v) {
 
 //":"の形以外は飛ばす
@@ -76,44 +77,51 @@ foreach ($http_response_header as $v) {
 var_dump($cookies);
 
 // CookieからSessionIdを取得                            
-
 foreach ($cookies as $v) {
-    if (preg_match('/user_session=user_session_=(\w+)/', $v, $session_id)) {
+
+    if (preg_match('/user_session=user_session/', $v)) {
+        // /user_session=user_session_が含まれる場合
+        //user_session=user_session_~;までが必要
+
+
+        $session_id = mb_strstr(trim($v),' ',true);
+
     }
 }
 
 
 var_dump($session_id);
 
+//ログイン後にやりたい処理
+// コメントサーバー情報の取得
+$method = 'POST';
+
+$request = array();
+
+$query = http_build_query($request, '', '&');
+
+//リクエストヘッダ
+$header = array(
+    "Content-Type: application/x-www-form-urlencoded",
+    "Content-Length: " . strlen($query),
+    "Cookie: " . "Cookie: " . $session_id, 
+    "User-Agent: " . "hogehoge", 
+);
+
+$context = array(
+    "http" => array(
+        "method" => $method,
+        "header" => implode("\r\n", $header),
+        "content" => $query,
+    )
+);
 
 
 
-// // POST用関数
-// function http_post ($url, $data)
-// {
-//   $data_url = http_build_query ($data);
-//   $data_len = strlen ($data_url);
- 
-//   return array (
-//         'content'=>  file_get_contents (
-//             $url,
-//             false,
-//             stream_context_create (
-//               array ('http' =>
-//                   array (
-//                       'method'=>'POST',
-//                       'header'=>'Content-Type: application/x-www-form-urlencoded\r\nContent-Length: $data_len\r\n',
-//                       'content'=>$data_url)
-//                   )
-//               )
-//             ),
-//         'headers'=> $http_response_header
-//   );
+$response = file_get_contents($ply_sts_url.$live_id, false, stream_context_create($context));
 
-// }
+var_dump($response);
 
-// // 送信
-// $result = http_post($url, $data);
 
 
 
