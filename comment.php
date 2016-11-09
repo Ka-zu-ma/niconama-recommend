7e7479d0d5872d6253aec9207f324a7ec75d94f3
Kazuma Miyazaki <miyazaki@dotinstall.com>
2016-11-10 01:34:32 +0900

    1109commit

--- comment.php
+++ comment.php
@@ -1,19 +1,13 @@
 <?php
 
-// ログイン画面URL、初め、URLが間違っていて、ログインできていなかったので要注意.
-$login_url = 'https://account.nicovideo.jp/api/v1/login?show_button_twitter=1&site=niconico&show_button_facebook=1&next_url=';
+define('NICONICO_MAILADDRESS','');
+define('NICONICO_PASSWORD', '');
+define('NICONICO_LOGIN_URL', 'https://account.nicovideo.jp/api/v1/login?show_button_twitter=1&site=niconico&show_button_facebook=1&next_url=');
+define('NICONICO_GETPLAYERSTATUS_URL', 'http://live.nicovideo.jp/api/getplayerstatus?v=');
 
-//プレイヤー情報を得るためのリクエストURL
-$ply_sts_url = 'http://live.nicovideo.jp/api/getplayerstatus?v=';
 
 // 放送ID
-$live_id = '';
-
-
-//ニコニコに登録してるメルアド
-$mail_tel = '';
-// ニコニコに登録してるパスワード
-$password = '';
+$live_id = 'lv';
 
 $cookies = array();
 
@@ -22,8 +16,8 @@ $session_id = '';
 //ログイン処理
 $method = 'POST';
 
-$request = array('mail_tel' => $mail_tel, 
-            'password' => $password
+$request = array('mail_tel' => NICONICO_MAILADDRESS, 
+            'password' => NICONICO_PASSWORD
 );
 
 $query = http_build_query($request, '', '&');
@@ -44,9 +38,7 @@ $context = array(
     )
 );
 
-
-
-$response = file_get_contents($login_url, false, stream_context_create($context));
+$response = file_get_contents(NICONICO_LOGIN_URL, false, stream_context_create($context));
 
 // 存在しないURLにリクエストを出したとき、エラー表示
 if($http_response_header[0] == 'HTTP/1.1 404 Not Found'){
@@ -55,7 +47,6 @@ if($http_response_header[0] == 'HTTP/1.1 404 Not Found'){
 
 var_dump($http_response_header);
 
-// クッキーを取得
 // file_get_contents後に$http_response_headerにレスポンスヘッダが格納されているのでここからクッキー情報を取得
 foreach ($http_response_header as $v) {
 
@@ -81,16 +72,11 @@ foreach ($cookies as $v) {
         // /user_session=user_session_が含まれる場合
         //user_session=user_session_~;までが必要
 
-
         $session_id = mb_strstr(trim($v),' ',true);
-
     }
 }
 
-
 var_dump($session_id);
-
-
 
 //ログイン後にやりたい処理
 // コメントサーバー情報の取得
@@ -117,14 +103,9 @@ $context = array(
     )
 );
 
-
-
-$response = file_get_contents($ply_sts_url.$live_id, false, stream_context_create($context));
-
+$response = file_get_contents(NICONICO_GETPLAYERSTATUS_URL.$live_id, false, stream_context_create($context));
 
 var_dump($response);
-
-
 
 //XMLをオブジェクトに変換
 $xmlObj = simplexml_load_string($response);
@@ -135,90 +116,21 @@ $xmlAry = json_decode( json_encode($xmlObj),true);
 // コメントサーバーURL
 $addr = $xmlAry['ms']['addr'];
 
-$addr = 'o'.$addr;
-
-
 // ポート
 $port = $xmlAry['ms']['port'];
 //スレッド
 $thread = $xmlAry['ms']['thread'];
 
-
 var_dump($xmlAry);
-
 
 //送信するメッセージ
 // res_fromは新しい順に何件前までを取得するかを指定 -1から-1000まで
 // res_fromに不正な数値が入力された場合は、-250が設定される。
 // 投稿者コメントを取得する時は、これにfork="1"を追加する:
-
-// $msg_format = '<thread thread="%s" version="(20061206|20090904)" res_from="-10" />\0';
-
-$msg_format = '<thread thread="%s" version="20061206" res_from="-10"/>\0';
-
-
+$msg_format = '<thread thread="%s" version="(20061206|20090904)" res_from="-10" scores="1"/>\0';
 
 
 $msg = sprintf($msg_format,(string)$thread);
-
-// $msg = '<thread thread="'.$thread.'" version="(20061206|20090904)" res_from="-1"/>';
-
-
-// $msg = '<thread thread="'.$thread.'" version="20061206" res_from="-10"/>';
-
-
-
-/*
-f~関数使ってる
-*/
-
-
-
-//とりあえずホスト名からソケットをオープンする
-$socket = fsockopen((string)$addr,(string)$port,$errno,$errstr,30);
-
-stream_set_blocking($socket, 0);
-
-
-//オープンしたソケットに上記のリクエストを書き込む(※送信する)
-fwrite($socket,$msg);
-
-
- $buffer = '';
-
-while(1){
-
-    $buffer .= fread($socket, 1024);
-
-}
-
-
-
- //ソケットを閉じる
- fclose($socket);
-
-var_dump($errstr);
-
-
-exit();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -232,16 +144,7 @@ $res = '';
 // TCP/IP ソケット作成
 $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 
-// $remote = socket_accept($socket);
-
-// socket_set_nonblock($remote); // リモートソケットをノンブロッキングモードにする
-
 if ($socket == true) {
-
-    // 送信タイムアウト時間の設定(10秒)
-    // $timeout = array('sec' => 10, 'usec' => 0);
-    // socket_set_option($socket, SOL_SOCKET, SO_SNDTIMEO, $timeout);
-
 
     // ソケット接続
     $connect_result = socket_connect($socket,$addr,$port);
@@ -256,7 +159,6 @@ var_dump($write_result);
 
         if ($write_result == true){
 
-            //PHP_EOL:改行
             $res = '送信しました。'.PHP_EOL;
 
         }else{
@@ -267,13 +169,9 @@ var_dump($write_result);
             
         }
 
-
-
     }else{
 
         $res = 'socket_connect() 失敗: '.socket_strerror(socket_last_error($socket)).PHP_EOL;
-
-
     }
 
 }else{
@@ -284,10 +182,8 @@ var_dump($write_result);
 }
 var_dump($res);
 
-// $read_result = socket_read($socket, 2048);
 
 $buf = 'This is my buffer.';
-
 
 if (false !== ($bytes = socket_recv($socket, $buf, 2048, MSG_DONTWAIT))) {
     echo "Read $bytes bytes from socket_recv(). Closing socket...";
@@ -296,96 +192,37 @@ if (false !== ($bytes = socket_recv($socket, $buf, 2048, MSG_DONTWAIT))) {
     echo "socket_recv() failed; reason: " . socket_strerror(socket_last_error($socket)) . "\n";
 }
 
-
-
-
-// while($read_result = socket_read($socket, 1024)){
-
-//     $data .= $read_result; 
-
-
-// };
-
 socket_close($socket);
-
-
 
 var_dump($buf);
 
 
 
 
-// var_dump($read_result);
+// f~関数使ったやりかた
 
 
+//とりあえずホスト名からソケットをオープンする
+// $socket = fsockopen((string)$addr,(string)$port,$errno,$errstr,30);
+
+// stream_set_blocking($socket, 0);
 
 
+// //オープンしたソケットに上記のリクエストを書き込む(※送信する)
+// fwrite($socket,$msg);
 
 
-// $buf = 'This is my buffer.';
+//  $buffer = '';
 
-// if (false !== ($bytes = socket_recv($socket, $buf, 2048, MSG_WAITALL))) {
+// while(1){
 
-//     echo "Read $bytes bytes from socket_recv(). Closing socket...";
-// } else {
-//     echo "socket_recv() failed; reason: " . socket_strerror(socket_last_error($socket)) . "\n";
+//     $buffer .= fread($socket, 1024);
+
 // }
 
+//  fclose($socket);
 
-
-
-
-
-
-
-
-
-
-
-
-// var_dump($bytes);
-
-// var_dump($buf);
-
-
-
-
-// $request = array('port' => $port,
-//                 'thread' => $thread
-//                 );
-// $method = 'POST';
-
-// $query = http_build_query($request, '', '&');
-
-// //リクエストヘッダ
-// $header = array(
-//     "Content-Type: application/x-www-form-urlencoded",
-//     "Content-Length: " . strlen($query),
-//     "Cookie: " . $session_id, 
-//     "User-Agent: " . "hogehoge", 
-// );
-
-// $context = array(
-//     "http" => array(
-//         "method" => $method,
-//         "header" => implode("\r\n", $header),
-//         "content" => $query,
-//     )
-// );
-
-
-
-
-
-// $response = file_get_contents($addr, false, stream_context_create($context));
-
-
-
-
-
-
-
-
+// var_dump($errstr);
 
 
 
